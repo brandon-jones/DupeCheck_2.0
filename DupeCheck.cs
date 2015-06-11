db5e930b2207ce1049a17abb6d37ba7226b98dbc
Brandon Jones <brandon.jones9@gmail.com>
2015-06-11 00:02:34 -0700

    init

---
+++ DupeCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DupeCheck_2._0
+{
+    public partial class DupeCheck : Form
+    {
+        public DupeCheck()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
